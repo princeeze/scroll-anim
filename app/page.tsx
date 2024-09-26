261aa18149129bbe2991803dad1bc911c0753e2b
Prince Eze <pchidireeze@gmail.com>
2024-09-26 18:58:35 +0100

    feat: add scroll anim

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,93 @@
+"use client";
+
 import Image from "next/image";
+import img1 from "@/public/1.jpg";
+import img2 from "@/public/2.jpg";
+import img3 from "@/public/3.jpg";
+import { useEffect, useRef } from "react";
+import Lenis from "lenis";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
 
-export default function Home() {
+export default function ScrollPage() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  const container = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <main className="overflow-hidden">
+      <div className="h-[100vh]"></div>
+      <div ref={container}>
+        <Slider
+          src={img1.src}
+          text="Front End Developer"
+          left="-12%"
+          progress={scrollYProgress}
+          direction="left"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Slider
+          src={img2.src}
+          text="Product Designer"
+          left="-15%"
+          progress={scrollYProgress}
+          direction="right"
+        />
+        <Slider
+          src={img3.src}
+          text="Web Developer"
+          left="-20%"
+          progress={scrollYProgress}
+          direction="left"
+        />
+      </div>
+      <div className="h-[100vh]"></div>
+    </main>
   );
 }
+
+const Slider = ({
+  src,
+  text,
+  left,
+  progress,
+  direction,
+}: {
+  src: string;
+  text: string;
+  left: string;
+  progress: MotionValue<number>;
+  direction: "left" | "right";
+}) => {
+  const dirX = direction === "left" ? -1 : 1;
+  const x = useTransform(progress, [0, 1], [-400 * dirX, 400 * dirX]);
+  return (
+    <motion.div className="flex whitespace-nowrap relative" style={{ left, x }}>
+      <Phrase src={src} text={text} />
+      <Phrase src={src} text={text} />
+      <Phrase src={src} text={text} />
+    </motion.div>
+  );
+};
+
+const Phrase = ({ src, text }: { src: string; text: string }) => {
+  return (
+    <div className="px-5 flex gap-5 items-center">
+      <p className="text-[7.5vw]">{text}</p>
+      <span className="relative h-[7.5vw] aspect-[4/2] rounded-full overflow-hidden">
+        <Image style={{ objectFit: "cover" }} src={src} alt="image" fill />
+      </span>
+    </div>
+  );
+};
