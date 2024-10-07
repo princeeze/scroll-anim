32749feb9bda294968ea87cc2771b216e34ded6c
Prince Eze <pchidireeze@gmail.com>
2024-10-07 18:51:31 +0100

    fix: hide canvas on mobile
    requires pointer to work

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ export default function Projects() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <Link href="/parallax">Parallax</Link>
-      <Link href="/canvas">Canvas</Link>
+      <Link href="/canvas" className="hidden lg:block">
+        Canvas
+      </Link>
       <Link href="/scroll">Scroll</Link>
       <Link href="/zoom">Zoom</Link>
       <Link href="/scroll-parallax">Scroll Parallax</Link>
+      <Link href="/box">Box</Link>
     </div>
   );
 }
